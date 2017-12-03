b2a33500449355b9c8d88224ede3b25d8cbaa006
Olena Ageyeva <eageeva@hotmail.com>
2017-12-03 20:23:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,6 +43,12 @@ class Dog
   end
 
   def self.find_by_id(id)
+    sql = <<-SQL
+     SELECT *
+     FROM dogs
+     WHERE id = ?     
+   SQL
+   DB[:conn].execute(sql, id)[0][0]        
   end
 
 end
